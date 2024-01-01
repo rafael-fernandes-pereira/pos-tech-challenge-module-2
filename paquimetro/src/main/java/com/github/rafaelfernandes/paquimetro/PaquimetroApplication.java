daed55b0c0005113e50c794d0600b804332ca64c
Rafael Fernandes <rafael.ferper@gmail.com>
2024-01-01 20:46:14 -0300

    Inicio do projeto

---
+++ paquimetro/src/main/java/com/github/rafaelfernandes/paquimetro/PaquimetroApplication.java
@@ -0,0 +1,13 @@
+package com.github.rafaelfernandes.paquimetro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaquimetroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaquimetroApplication.class, args);
+	}
+
+}
---
+++ paquimetro/src/test/java/com/github/rafaelfernandes/paquimetro/PaquimetroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rafaelfernandes.paquimetro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaquimetroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
